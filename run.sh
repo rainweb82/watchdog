23ab8b1780b430e7720d149665eb1318d18e7cf1
rainweb82 <105602643+rainweb82@users.noreply.github.com>
2022-06-13 16:01:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 source ./config
 rm -rf watchdog
-git clone --depth 1 $hub watchdog
+echo 正在更新脚本，请稍后...
+git clone --depth 1 $hub watchdog --quiet
 cp ./watchdog/run.sh run.sh
 bash ./watchdog/watchdog.sh
