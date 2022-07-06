5e89ed8c9fc3ac0f0bb7cc7b0ccb7fca5a2e367e
rainweb82 <105602643+rainweb82@users.noreply.github.com>
2022-07-06 19:56:22 +0800

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -11,9 +11,8 @@
 #v9 网站正常条件改为判断页面内容
 #v9.1 每日推送增加历史错误日志内容
 #v9.2 增加当前网络归属地的显示
+#v9.3 修改更新域名频率，调整错误日志输出
 
-#读取需监控的域名
-url=`cat ./watchdog/url.list`
 #读取配置文件
 source ./config
 
@@ -30,7 +29,7 @@ function loading()
 			sleep 0.25
 		fi
 		ratio_s=$(($ratio/4))
-		printf " \033[39m等待:[%-30s]%d秒[%c]    \r" "$markl" "$ratio_s" "${ch[$(($ratio%4))]}"
+		printf " \033[39m等待:[%-30s]%d秒[%c] \r" "$markl" "$ratio_s" "${ch[$(($ratio%4))]}"
 		markl=${mark:0:$((${#mark}/($1*60/30*4)+1))}
 		mark="#$mark"
 	done
@@ -57,9 +56,22 @@ function urlEncode() {
 #Pushplus推送
 function sendmsg()
 {
-nowmsg=http://www.pushplus.plus/send?token=$1$2
-nowmsgcode=`curl -o /dev/null --retry 3 --retry-max-time 30 -s -w %{http_code} $nowmsg`
-echo -e "\033[34m"本次PUSHPLUS推送已完成！代码:$nowmsgcode
+if [ ! $pushplustokena ]
+then
+    echo -e "\033[34m"未设置PUSHPLUS-A，跳过本次推送任务
+else
+    nowmsg=http://www.pushplus.plus/send?token=$pushplustokena$1
+    nowmsgcode=`curl -o /dev/null --retry 3 --retry-max-time 30 -s -w %{http_code} $nowmsg`
+    echo -e "\033[34m"本次PUSHPLUS推送已完成！代码:$nowmsgcode
+fi
+if [ ! $pushplustokenb ]
+then
+    echo -e "\033[34m"未设置PUSHPLUS-B，跳过本次推送任务
+else
+    nowmsg=http://www.pushplus.plus/send?token=$pushplustokenb$1
+    nowmsgcode=`curl -o /dev/null --retry 3 --retry-max-time 30 -s -w %{http_code} $nowmsg`
+    echo -e "\033[34m"本次PUSHPLUS推送已完成！代码:$nowmsgcode
+fi
 }
 #获取本机ip归属地信息
 function ipp
@@ -72,13 +84,13 @@ uipp="`urlEncode $ipp`"
 #更新检测url
 function updateurl
 {
-if [[ $urlhub != "" ]]
+if [[ ! $urlhub ]]
 then
-	new_url="`curl --retry 3 --retry-max-time 30 -L -s $urlhub`"
-else
 	rm -rf watchdog
 	git clone --depth 1 $hub watchdog --quiet
 	new_url=`cat ./watchdog/url.list`
+else
+	new_url="`curl --retry 3 --retry-max-time 30 -L -s $urlhub`"
 fi
 }
 
@@ -90,10 +102,13 @@ lxcwhj=0
 issend=0
 wrong=''
 tstart=`date '+%s'`
-autograph="%3Cbr+%2F%3E%3Cbr+%2F%3E%E6%9C%AC%E9%80%9A%E7%9F%A5+By%EF%BC%9Agithub.com%2Frainweb82%2Fwatchdog"
+autograph="%3Cbr+%2F%3E%3Cbr+%2F%3Ehttp%3A%2F%2Fgithub.com%2Frainweb82%2Fwatchdog"
 #如填写了urlhub，则使用此地址的url进行检测
-if [[ $urlhub != "" ]]
+if [[ ! $urlhub ]]
 then
+#读取需监控的域名
+    url=`cat ./watchdog/url.list`
+else
 	url="`curl --retry 3 --retry-max-time 30 -L -s $urlhub`"
 fi
 #推送容错时间计算
@@ -127,7 +142,7 @@ do
 	if [[ $url == "stop" ]]
 	then
 		echo -e "\033[31m"接受到停止指令，暂停域名监控！"\033[30m"
-		loading 60
+		loading 120
 		bash ./watchdog/watchdog.sh
 	fi
 	#每日推送
@@ -147,24 +162,6 @@ do
 	if [ $(( $(($zcnum+$cwnum)) % $tjnum )) = 0 ] && [ $(($zcnum+$cwnum)) -ne 0 ]
 	then
 		echo -e "\033[35m"已检测:$(($zcnum+$cwnum))次 正常:$zcnum次 错误:$cwnum次 运行:$day天$hour小时$min分$sec秒
-	fi
-	#判断是否发送每日推送
-	if [ $nowtime -eq $daypost ] && [ $issend -eq 1 ]
-	then
-		nowmsg=\&title=$surl%E7%9B%91%E6%8E%A7%E6%97%A5%E6%8A%A5\&content=%E7%9B%91%E6%8E%A7%E5%9F%9F%E5%90%8D%EF%BC%9A$surl%3Cbr+%2F%3E%E7%B4%AF%E8%AE%A1%E7%9B%91%E6%8E%A7%EF%BC%9A$(($zcnum+$cwnum))%E6%AC%A1+%E3%80%90%E6%AD%A3%E5%B8%B8%EF%BC%9A$zcnum%E6%AC%A1%EF%BC%8C%E9%94%99%E8%AF%AF%EF%BC%9A$cwnum%E6%AC%A1%E3%80%91%3Cbr+%2F%3E%E8%BF%90%E8%A1%8C%E6%97%B6%E9%97%B4%EF%BC%9A$day%E5%A4%A9$hour%E5%B0%8F%E6%97%B6$min%E5%88%86$sec%E7%A7%92%3Cbr+%2F%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp%3Cbr+%2F%3E`date +"%m-%d_%H:%M:%S"`%3Cbr+%2F%3E$wrong$autograph\&template=html
-		if [ ! $pushplustokena ]
-		then
-			echo -e "\033[34m"未设置PUSHPLUS-A，跳过本次每日推送任务
-		else
-			sendmsg $pushplustokena $nowmsg
-		fi
-		if [ ! $pushplustokenb ]
-		then
-			echo -e "\033[34m"未设置PUSHPLUS-B，跳过本次每日推送任务
-		else
-			nsendmsg $pushplustokenb $nowmsg
-		fi
-		issend=0
 		#定时检查域名是否有更新
 		updateurl
 		if [ $url != $new_url ]
@@ -177,10 +174,23 @@ do
 			jjurl $url
 			echo -e "\033[33m"更新域名为:$url
 		else
-			echo -e "\033[33m"域名无变化，继续监控:$url
+			echo -e "\033[33m"域名无更新，继续监控:$url
 		fi
 		#更新运行文件
 		cp ./watchdog/run.sh run.sh
+	fi
+	#判断是否发送每日推送
+	if [ $nowtime -eq $daypost ] && [ $issend -eq 1 ]
+	then
+		if [ ! $wrong ]
+		then
+			wrongmsg=%3Cbr+%2F%3E%E6%9A%82%E6%97%A0%E9%94%99%E8%AF%AF%E6%97%A5%E5%BF%97
+		else
+			wrongmsg=%3Cbr+%2F%3E%E9%94%99%E8%AF%AF%E6%97%A5%E5%BF%97%EF%BC%9A${wrong:0:84*20}
+		fi
+		nowmsg=\&title=$surl%E7%9B%91%E6%8E%A7%E6%97%A5%E6%8A%A5\&content=%E7%9B%91%E6%8E%A7%E5%9F%9F%E5%90%8D%EF%BC%9A$surl%3Cbr+%2F%3E%E7%B4%AF%E8%AE%A1%E7%9B%91%E6%8E%A7%EF%BC%9A$(($zcnum+$cwnum))%E6%AC%A1+%E3%80%90%E6%AD%A3%E5%B8%B8%EF%BC%9A$zcnum%E6%AC%A1%EF%BC%8C%E9%94%99%E8%AF%AF%EF%BC%9A$cwnum%E6%AC%A1%E3%80%91%3Cbr+%2F%3E%E8%BF%90%E8%A1%8C%E6%97%B6%E9%97%B4%EF%BC%9A$day%E5%A4%A9$hour%E5%B0%8F%E6%97%B6$min%E5%88%86$sec%E7%A7%92%3Cbr+%2F%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp%3Cbr+%2F%3E`date +"%m-%d_%H:%M:%S"`%3Cbr+%2F%3E$wrongmsg$autograph\&template=html
+		sendmsg $nowmsg
+		issend=0
 	fi
 	date=`date +"%m-%d %H:%M:%S"`
 	#判断网站源码是否包含指定内容
@@ -213,18 +223,7 @@ do
 		if [ $(( $times % $msgtimes )) = 0 ] && [ $times -ne 0 ] ; then
 			#推送消息
 			nowmsg=\&title=$surl%E7%BD%91%E7%AB%99%E6%8C%82%E4%BA%86\&content=$surl+%E5%9F%9F%E5%90%8D%E6%8C%82%E4%BA%86%EF%BC%8C%E5%BF%AB%E5%8E%BB%E7%9C%8B%E7%9C%8B%3Cbr+%2F%3E%E7%BD%91%E7%BB%9C%E5%BD%92%E5%B1%9E%EF%BC%9A$uipp%3Cbr+%2F%3E`date +"%m-%d_%H:%M:%S"`$autograph\&template=html
-			if [ ! $pushplustokena ]
-			then
-				echo -e "\033[34m"未设置PUSHPLUS-A，跳过本次错误推送任务
-			else
-				sendmsg $pushplustokena $nowmsg
-			fi
-			if [ ! $pushplustokenb ]
-			then
-				echo -e "\033[34m"未设置PUSHPLUS-B，跳过本次错误推送任务
-			else
-				sendmsg $pushplustokenb $nowmsg
-			fi
+			sendmsg $nowmsg
 			#重置报错计数
 			times=0
 			temptimes=0
@@ -251,7 +250,7 @@ do
 				echo -e "\033[33m"更新域名为:$url
 				break
 			fi
-			echo -e "\033[33m"域名未更新，等待30分钟
+			echo -e "\033[33m"域名无更新，等待30分钟
 			loading 30
 		done
 		echo -e "\033[39m"重新开始域名检测 $date
